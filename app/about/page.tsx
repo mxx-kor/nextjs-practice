6abc652b52daa7b4f958ac5ac52f634071b10cf3
mxx-kor <minjae7728@gmail.com>
2023-08-09 23:16:40 +0900

    feat: SSG 페이지 구현해보기

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
-const About = () => {
-  return <h1>About</h1>;
+interface IData {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+//SSG
+export const metadata = {
+  title: 'about my app',
+  description: '저희 팀과 통계를 소개합니다.',
+};
+
+const getData = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+};
+
+const About = async () => {
+  const data: IData = await getData();
+  return (
+    <main>
+      <h1>About</h1>
+      <div>
+        <p>{data.id}</p>
+        <p>{data.title}</p>
+        <p>{data.body}</p>
+      </div>
+    </main>
+  );
 };
 
 export default About;
