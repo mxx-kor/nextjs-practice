af633501b43378bb87fc1d425a7b218c1ff8520e
mxx-kor <minjae7728@gmail.com>
2023-08-09 23:16:51 +0900

    feat: ISR 페이지 구현해보기

--- app/about/@analytics/page.tsx
+++ app/about/@analytics/page.tsx
@@ -1,5 +1,43 @@
-const Analytics = () => {
-  return <div>Analytics</div>;
+interface IComment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+const getPostComments = async (id: number) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}/comments`,
+    {
+      next: {
+        revalidate: 10, // 데이터 유효 시간 10초
+      },
+    },
+  );
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+};
+
+//ISR
+const Analytics = async () => {
+  const comments: IComment[] = await getPostComments(1);
+  return (
+    <>
+      <h2>Analytics</h2>
+      {comments.map((comment) => (
+        <div key={comment.id}>
+          <p>name: {comment.name}</p>
+          <p>{comment.body}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Analytics;
