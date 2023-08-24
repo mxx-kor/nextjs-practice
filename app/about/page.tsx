bfd71c9f4aa7262d3b030e9c17cedbcb34edc162
mxx-kor <minjae7728@gmail.com>
2023-08-25 00:19:53 +0900

    feat: route handler 적용해보기

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,4 @@
-import React from 'react';
-
-interface IData {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
+import {Card, Divider} from '@mui/material';
 
 //SSG
 export const metadata = {
@@ -13,26 +6,42 @@ export const metadata = {
   description: '저희 팀과 통계를 소개합니다.',
 };
 
-const getData = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+const getPosts = async () => {
+  const res = await fetch('http://localhost:3000/api/about');
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data');
   }
 
-  return res.json();
+  return (await res.json()) as Post[];
 };
 
 const About = async () => {
-  const data: IData = await getData();
+  const posts = await getPosts();
+
   return (
     <main>
       <h1>About</h1>
       <div>
-        <p>{data.id}</p>
-        <p>{data.title}</p>
-        <p>{data.body}</p>
+        {posts.map((post) => (
+          <Card key={post.id}>
+            <h2>{post.title}</h2>
+            <p>
+              By {post.author} | {post.date}
+            </p>
+            <p>{post.content}</p>
+            <div>
+              {post.comments.map((comment) => (
+                <div key={comment.id}>
+                  <p>{comment.author}</p>
+                  <p>{comment.text}</p>
+                </div>
+              ))}
+            </div>
+            <Divider />
+          </Card>
+        ))}
       </div>
     </main>
   );
